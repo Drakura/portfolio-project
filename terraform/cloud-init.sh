a08db38e99a6c79419143b56e9f83a5454779f30
Vincent Michalka <lvmichalka@gmail.com>
2026-09-11 14:35:37 -0400

    Fix Docker installation in cloud-init

--- terraform/cloud-init.sh
+++ terraform/cloud-init.sh
@@ -1,9 +1,40 @@
 #!/bin/bash
+set -e
+
 apt-get update -y
-apt-get install -y docker.io
+apt-get install -y ca-certificates curl
+
+install -m 0755 -d /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+	-o /etc/apt/keyrings/docker.asc
+
+chmod a+r /etc/apt/keyrings/docker.asc
+
+cat > /etc/apt/sources.list.d/docker.sources <<EOF
+
+	Types: deb
+	URIs: https://download.docker.com/linux/ubuntu
+	Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+	Components: stable
+	Architectures: $(dpkg --print-architecture)
+	Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+apt-get update -y
+
+apt-get install -y \
+	docker-ce \
+	docker-ce-cli \
+	containerd.io \
+	docker-buildx-plugin \
+	docker-compose-plugin
 
 systemctl enable docker
 systemctl start docker
 
 docker pull ${image_name}
-docker run -d -p ${external_port}:80 --name ${container_name} ${image_name}
+
+docker run -d \
+	-p ${external_port}:80 \
+	--name ${container_name} ${image_name}
